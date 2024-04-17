02979180e9e4d96043dee17cb7579a6b47f93342
Hemant Yadav <1134hemant@gmail.com>
2024-04-17 10:42:17 +0530

    ...

--- Frontend/src/Components/Cart.jsx
+++ Frontend/src/Components/Cart.jsx
@@ -1,272 +1,179 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+"use client";
+import React, { useEffect, useState } from 'react'
+import useCartContext from './Context/CartContext';
+import Link from 'react-router-dom';
 
-const Cart = () => {
+import { useParams } from 'react-router-dom';
+
+const CartPage = () => {
+
+  const { id } = useParams();
+
+  const send = (e) => {
+    dispatch(addToCart(e))
+    toast.success("Item added In Your Cart")
+  }
+  const [productList, setproductList] = useState([]);
+
+  const fetchUserData = async () => {
+    const res = await fetch('http://localhost:3000/product/getall');
+
+    console.log(res.status);
+    if (res.status === 200) {
+      const data = await res.json();
+      console.log(data);
+      setproductList(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, [])
+
+
+  const { setCartOpen } = useCartContext();
+  const {
+    cartItems,
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    isInCart,
+    getCartTotal,
+    getCartItemsCount,
+  } = useCartContext();
+
+  const displayCartItems = () => {
+    if (getCartItemsCount() === 0) return (
+      <div className="text-center">
+        {/* <p alt="login form" className='rounded-start mt-4 w-25 text-center' /> */}
+        <h3>Your Cart is Currently Empty!</h3>
+        <p className="text-muted">Before proceed to checkout you must add some products to your shopping cart. <br />You will fill a lot of interesting products on our "Product" page.</p>
+        <Link className="btn rounded-pill" onClick={() => setCartOpen(false)} style={{ backgroundColor: "#4BCCF2", color: "#fff" }} href={"/product"}>Return To Shop</Link>
+      </div>
+    );
+    return cartItems.map((item) => (
+      <div key={item._id} className="grid grid-cols-3 mb-4">
+        <div className="">
+          {/* <img src={'http://localhost:5000/' + item.image} alt="" className="w-auto mx-auto  h-24  py-1 " style={{ objectFit: "cover" }} /> */}
+
+
+        </div>
+        <div className="">
+          <p className="text-muted h6">{item.pname}</p>
+          <h3>{item.title}</h3>
+          <p className="text-muted">{item.pcategory}</p>
+        </div>
+        <div className="">
+          <div className="input-group flex">
+            <button className="bg-gray-400 rounded text-xl text-white px-3 py-2" onClick={e => addItemToCart(item)}>+ </button>
+            <input type="text" className=" w-12 px-2 text-center" value={item.quantity} />
+            <button className="bg-gray-400 rounded text-xl text-white px-3 py-2" onClick={e => removeItemFromCart(item)}>-</button>
+        
+          </div>
+          <h2 className="my-2"> ₹ {item.pprice}</h2>
+        </div>
+      </div>
+    ));
+  }
   return (
-    <section className="h-100" style={{ backgroundColor: "#eee" }}>
-  <div className="container h-100 py-5">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-10">
-        <div className="d-flex justify-content-between align-items-center mb-4">
-          <h3 className="fw-normal mb-0 text-2xl text-black">Shopping Cart</h3>
-          </div>
-        <div className="card rounded-3 mb-4">
-          <div className="card-body p-4">
-            <div className="row d-flex justify-content-between align-items-center">
-              <div className="col-md-2 col-lg-2 col-xl-2">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                  className="img-fluid rounded-3"
-                  alt="Cotton T-shirt"
-                />
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-3">
-                <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                <p>
-                  <span className="text-muted">Size: </span>M{" "}
-                  <span className="text-muted">Color: </span>Grey
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                >
-                  <i className="fas fa-minus" />
-                </button>
-                <input
-                  id="form1"
-                  min={0}
-                  name="quantity"
-                  defaultValue={2}
-                  type="number"
-                  className="form-control form-control-sm"
-                />
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                >
-                  <i className="fas fa-plus" />
-                </button>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 className="mb-0">$499.00</h5>
-              </div>
-              <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                <a href="#!" className="text-danger">
-                  <i className="fas fa-trash fa-lg" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card rounded-3 mb-4">
-          <div className="card-body p-4">
-            <div className="row d-flex justify-content-between align-items-center">
-              <div className="col-md-2 col-lg-2 col-xl-2">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                  className="img-fluid rounded-3"
-                  alt="Cotton T-shirt"
-                />
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-3">
-                <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                <p>
-                  <span className="text-muted">Size: </span>M{" "}
-                  <span className="text-muted">Color: </span>Grey
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                >
-                  <i className="fas fa-minus" />
-                </button>
-                <input
-                  id="form1"
-                  min={0}
-                  name="quantity"
-                  defaultValue={2}
-                  type="number"
-                  className="form-control form-control-sm"
-                />
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                >
-                  <i className="fas fa-plus" />
-                </button>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 className="mb-0">$499.00</h5>
-              </div>
-              <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                <a href="#!" className="text-danger">
-                  <i className="fas fa-trash fa-lg" />
-                </a>
+    <>
+
+
+      <div
+        className="relative z-10"
+        aria-labelledby="slide-over-title"
+        role="dialog"
+        aria-modal="true"
+      >
+
+
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+
+              <div className="pointer-events-auto w-screen max-w-md">
+                <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                    <div className="flex items-start justify-between">
+                      <h2
+                        className="text-lg font-medium text-gray-900"
+                        id="slide-over-title"
+                      >
+                        Shopping cart
+                      </h2>
+                      <div className="ml-3 flex h-7 items-center">
+                        <button
+                          type="button"
+                          onClick={() => setCartOpen(false)}
+                          className="relative -m-2 p-2  text-gray-400 hover:text-gray-500"
+                        >
+                          <span className="absolute -inset-0.5" />
+                          <span className="sr-only">Close panel</span>
+                          <svg
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="mt-8">
+                      <div className="flow-root">
+                        <ul role="list" className="-my-6 divide-y divide-gray-200">
+                          <li className="flex py-6">
+
+                            <div className="ml-4 flex flex-1 flex-col">
+                              {displayCartItems()}
+                            </div>
+                          </li>
+
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                    <div className="flex justify-between text-base font-medium text-gray-900">
+                      <p>Total: {getCartTotal()}</p>
+                      <p>Items: {getCartItemsCount()}</p>
+                    </div>
+
+                    <button className="bg-red-600 w-full hover:bg-red-700 rounded py-1 text-white" onClick={() => clearCart()}> Clear Cart</button>
+                    <Link href='/user/checkout'><button className="bg-blue-500 text-white mt-2 hover:bg-blue-600 w-full py-1 rounded"   onClick={() => setCartOpen(false)} >Procees to Pay</button></Link>
+                    <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                      <p>
+                        or
+                        <button
+                          type="button"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                          Continue Shopping
+                          <span aria-hidden="true"> →</span>
+                        </button>
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="card rounded-3 mb-4">
-          <div className="card-body p-4">
-            <div className="row d-flex justify-content-between align-items-center">
-              <div className="col-md-2 col-lg-2 col-xl-2">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                  className="img-fluid rounded-3"
-                  alt="Cotton T-shirt"
-                />
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-3">
-                <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                <p>
-                  <span className="text-muted">Size: </span>M{" "}
-                  <span className="text-muted">Color: </span>Grey
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                >
-                  <i className="fas fa-minus" />
-                </button>
-                <input
-                  id="form1"
-                  min={0}
-                  name="quantity"
-                  defaultValue={2}
-                  type="number"
-                  className="form-control form-control-sm"
-                />
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                >
-                  <i className="fas fa-plus" />
-                </button>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 className="mb-0">$499.00</h5>
-              </div>
-              <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                <a href="#!" className="text-danger">
-                  <i className="fas fa-trash fa-lg" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card rounded-3 mb-4">
-          <div className="card-body p-4">
-            <div className="row d-flex justify-content-between align-items-center">
-              <div className="col-md-2 col-lg-2 col-xl-2">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                  className="img-fluid rounded-3"
-                  alt="Cotton T-shirt"
-                />
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-3">
-                <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                <p>
-                  <span className="text-muted">Size: </span>M{" "}
-                  <span className="text-muted">Color: </span>Grey
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                >
-                  <i className="fas fa-minus" />
-                </button>
-                <input
-                  id="form1"
-                  min={0}
-                  name="quantity"
-                  defaultValue={2}
-                  type="number"
-                  className="form-control form-control-sm"
-                />
-                <button
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-link px-2"
-                  onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                >
-                  <i className="fas fa-plus" />
-                </button>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 className="mb-0">$499.00</h5>
-              </div>
-              <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                <a href="#!" className="text-danger">
-                  <i className="fas fa-trash fa-lg" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-4">
-          <div className="card-body p-4 d-flex flex-row">
-            <div data-mdb-input-init="" className="form-outline flex-fill">
-              <input
-                type="text"
-                id="form1"
-                className="form-control form-control-lg"
-              />
-              <label className="form-label" htmlFor="form1">
-                Discount Code
-              </label>
-            </div>
-            <button
-              type="button"
-              data-mdb-button-init=""
-              data-mdb-ripple-init=""
-              className="btn btn-outline-warning btn-lg ms-3"
-            >
-              Apply
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <Link 
-              to='/Checkout'
-              type="button"
-              data-mdb-button-init=""
-              data-mdb-ripple-init=""
-              className="btn btn-warning btn-block btn-lg"
-            >
-             Confirm Checkout
-            </Link>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
 
+
+
+    </>
   )
 }
 
-export default Cart
+export default CartPage
